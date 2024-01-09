ca2a9154b4b2dac8c37a28756b2183c2881d05b7
Nyagol <nyagolpeter@gmail.com>
2024-01-09 12:22:31 +0300

    done

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+import os
+
+def create_grade_report(student_grades):
+    if not os.path.exists('reports'):
+        os.makedirs('reports')
+
+    file_path = os.path.join('reports', 'grade_reports.txt')
+    with open(file_path, 'w') as gr:
+        for grade in student_grades:
+            gr.write(grade + '\n')
 
 if __name__ == '__main__':
-    pass
+    student_grades = []
+
+    grade = input("Student Name, Grade: ")
+    while grade:
+        student_grades.append(grade)
+        grade = input("Student Name, Grade: ")
+
+    create_grade_report(student_grades)
